1e4c23fc07498bddd2ca3771005e54dc13336291
Matin Tamizi <matin@cuenca.io>
2018-06-05 16:52:24 -0500

    etl all billers

--- regalii_certifier/certifications/syncing_billers.py
+++ regalii_certifier/certifications/syncing_billers.py
@@ -1,3 +1,6 @@
+import json
+import threading
+
 from regalii_certifier import db
 from regalii_certifier.client import client
 from regalii_certifier.models import Biller
@@ -6,14 +9,24 @@ from regalii_certifier.models import Biller
 ENDPOINT = '/billers/credentials'
 
 
-def _get_page(page):
+def get_page(page):
     endpoint = f'{ENDPOINT}?page={page}'
     return client.biller.request(endpoint).get()
 
 
-def etl_billers():
-    billers = _get_page(1).data()
-    for biller_dict in billers['billers']:
+def etl_biller_page(page):
+    resp = get_page(page)
+    for biller_dict in resp.data()['billers']:
         biller = Biller.transform(biller_dict)
         db.Session.add(biller)
     db.Session.commit()
+
+
+def etl_billers():
+    resp = client.biller.credentials()
+    pages = json.loads(resp.pagination())['total_pages']
+    threads = []
+    for page in range(pages):
+        thread = threading.Thread(target=etl_biller_page, args=(page,))
+        thread.start()
+        threads.append(thread)
