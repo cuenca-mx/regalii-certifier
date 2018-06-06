d2ac8ae22572cc46ce7f93dae3ea3eeb53e7e4d4
Matin Tamizi <matin@cuenca.io>
2018-06-05 21:45:13 -0500

    use Mexican version

--- regalii_certifier/client.py
+++ regalii_certifier/client.py
@@ -6,8 +6,8 @@ from regalii.regaliator import Regaliator
 
 API_KEY = os.environ['REGALII_API_KEY']
 SECRET_KEY = os.environ['REGALII_SECRET_KEY']
-HOST = 'apix.casiregalii.com'
-VERSION = '3.2'
+HOST = 'api.casiregalii.com'
+VERSION = '3.1'
 
 config = Configuration(API_KEY, SECRET_KEY, HOST, version=VERSION)
 client = Regaliator(config)
